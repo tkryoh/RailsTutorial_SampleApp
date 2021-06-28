6f74d697be5201e92116e80d1cc9f50f34e2a52b
TKRyoh <hiro.ryoh@gmail.com>
2021-06-28 08:23:21 +0000

    Add mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,8 +73,9 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
+    :domain         => 'heroku.com',
     :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
